6336e9eff4f901bbf1e9d6f31e95c64157539d51
Steven Rachesky <steverachesky@gmail.com>
2017-03-26 21:06:18 -0400

    initial commit

---
+++ artix/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  artix
+//
+//  Created by Steven Rachesky on 3/26/17.
+//  Copyright © 2017 Rachesky-Brown. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
